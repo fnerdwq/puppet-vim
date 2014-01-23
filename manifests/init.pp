5b3134756078baf284538954cfef4ace39092d58
Frederik Wagner <frederik.wagner@gmx.de>
2014-01-23 15:00:08 +0100

    initial commit (really now)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-
+# simple class configuring vim defaults
 class vim {
   package {'vim': ensure => present }
 
@@ -8,27 +8,26 @@ class vim {
     package {'vim-puppet': ensure => present, require => Package['vim'] }
 
     exec { 'puppet syntax':
-     command => 'vim-addon-manager -w install puppet',
-     environment => ['HOME=/tmp'], # vim-addon-manager wants a set HOME dir (bug)
-     unless => 'vim-addon-manager -qw status puppet | grep -q installed',
-     path   => ["/bin", "/usr/bin"],
-
-     require => Package['vim', 'vim-puppet']
+      command     => 'vim-addon-manager -w install puppet',
+      environment => ['HOME=/tmp'], # vim-addon-manager wants a set HOME dir (bug)
+      unless      => 'vim-addon-manager -qw status puppet | grep -q installed',
+      path        => ['/bin', '/usr/bin'],
+      require     => Package['vim', 'vim-puppet']
     }
 
     file {'/etc/vim/vimrc.local':
-      owner  => root,
-      group  => root,
-      mode    => 0644,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
       source  => 'puppet:///vim/vimrc.local',
       require => Package['vim']
     }
   }
 
   file {'/etc/profile.d/vim.sh':
-    owner  => root,
-    group  => root,
-    mode    => 0644,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
     source  => 'puppet:///vim/vim.sh',
     require => Package['vim']
   }
