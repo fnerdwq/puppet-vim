984b684f224e811ac601d7704eb239de636f6d2d
Frederik Wagner <wagner@wagit.de>
2014-07-25 14:12:42 +0200

    added simple test for vim::add_on_mangaer

---
+++ spec/defines/addon_managner_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'vim::addon_manager' do
+    let(:title) { 'puppet' }
+
+    context "on Debian like systems" do
+	  let(:facts) { {:osfamily => 'Debian'} }
+      it { should contain_exec('enable vim syntax: puppet') }
+    end
+
+    context "on other systems" do
+	  let(:facts) { {:osfamily => 'RedHat'} }
+      it do
+        expect { should raise_error(Puppet::Error, /not supported/) }
+      end
+    end
+
+end
+
+
+at_exit { RSpec::Puppet::Coverage.report! }
+
